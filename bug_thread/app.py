5191193944b52b5e51050c13aacc3bb96f1eae11
crawford <crawford.collins@gmail.com>
2019-03-08 00:36:23 -0600

    login is kinda working routes for users and questions are working

---
+++ bug_thread/app.py
@@ -0,0 +1,106 @@
+from functools import wraps
+import json
+from os import environ as env
+from werkzeug.exceptions import HTTPException
+from decouple import config
+from .models import DB, User,Question,Answer
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask
+from flask import jsonify
+from flask import redirect
+from flask import render_template
+from flask import session
+from flask import url_for
+from authlib.flask.client import OAuth
+from six.moves.urllib.parse import urlencode
+
+
+def create_app():
+  app = Flask(__name__)
+  app.secret_key=config("SECRET_KEY")
+  app.config['SQLALCHEMY_DATABASE_URI'] = config("DATABASE_URL")
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  DB.init_app(app)
+  oauth = OAuth(app)
+
+  auth0 = oauth.register(
+    'auth0',
+    client_id=config("CLIENT_ID"),
+    client_secret=config("CLIENT_SECRET"),
+    api_base_url=config("API_BASE_URL"),
+    access_token_url=config("ACCESS_TOKEN_URL"),
+    authorize_url= config("AUTHORIZE_URL"),
+    client_kwargs={
+      'scope': 'openid profile',
+    },
+  )
+
+  def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+      if 'profile' not in session:
+        # Redirect to Login page here
+        return redirect('/')
+      return f(*args, **kwargs)
+
+    return decorated
+
+  @app.route('/')
+  def home():
+    return render_template('home.html')
+
+  @app.route('/callback')
+  def callback_handling():
+    # Handles response from token endpoint
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    # Store the user information in flask session.
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    db_user = (User.query.get(userinfo['sub']) or 
+        User(id=userinfo['sub'], name = userinfo['name']))
+    DB.session.add(db_user)
+    DB.session.commit()
+    return redirect('/dashboard')
+
+  @app.route('/login')
+  def login():
+      return auth0.authorize_redirect(redirect_uri='http://127.0.0.1:5000/callback', audience='https://crawftv.auth0.com/userinfo')
+
+  
+
+  @app.route('/dashboard')
+  @requires_auth
+  def dashboard():
+    return render_template('dashboard.html',
+                          userinfo=session['profile'],
+                          userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
+  
+
+  @app.route('/logout')
+  def logout():
+    # Clear session stored data
+    session.clear()
+    # Redirect user to logout endpoint
+    params = {'returnTo': url_for('home', _external=False), 'client_id': config('CLIENT_ID')}
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+
+  @app.route('/users')
+  @requires_auth
+  def display_users_page():
+    users = User.query.all()
+    return render_template('users.html',users = users)
+
+  @app.route('/q',methods=['POST','GET'])
+  @requires_auth
+  def display_questions():
+    questions = Question.query.all()
+    return render_template('questions.html',questions=questions)
+
+  return app
--- bug_thread/models.py
+++ bug_thread/models.py
@@ -3,25 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 DB = SQLAlchemy()
 
 class User(DB.Model):
-    id = DB.Column(DB.BigInteger, primary_key=True, unique =True)
+    id = DB.Column(DB.String, primary_key=True, unique =True)
     name = DB.Column(DB.String(30),nullable=False)
-    questions = DB.relationship('Question',backref='user',lazy=True)
-    answers = DB.relationship('Answer',backref='question',lazy=True)
+    
 
 
 class Question(DB.Model):
     id = DB.Column(DB.BigInteger,primary_key=True)
     text = DB.Column(DB.Unicode(1500))
-    user_id = DB.Column(DB.BigInteger,DB.ForeignKey('user.id'),nullable=False)
+    user_id = DB.Column(DB.String,DB.ForeignKey('user.id'),
+        nullable=False)
     solved_status = DB.Column(DB.Boolean,nullable=False)
     date = DB.Column(DB.DateTime)
     section = DB.Column(DB.String)
-    answers = DB.relationship('Answer',backref='question',nullable=False)
+    answers = DB.relationship('Answer',backref='questions',
+        lazy = True)
+    user = DB.relationship('User',backref='questions',lazy=True)
 
 class Answer(DB.Model):
     id =DB.Column(DB.BigInteger,primary_key=True)
     text = DB.Column(DB.Unicode(1500),nullable=False)
-    user_id = DB.Column(DB.BigInteger,DB.ForeignKey('user.id'),nullable=False)
+    user_id = DB.Column(DB.String,DB.ForeignKey('user.id'),
+        nullable=False)
     is_answer = DB.Column(DB.Boolean,nullable=False)
     date = DB.Column(DB.DateTime)
-
+    question_id = DB.Column(DB.BigInteger, DB.ForeignKey('question.id'),
+        nullable=False)
+    user = DB.relationship('User',backref='answers')
